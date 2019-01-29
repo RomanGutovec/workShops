705656940a0d47acbb1db73ee5d8c57a5d3150d6
Roman Gutovec <roman.gutovec@gmail.com>
2019-01-29 17:51:41 +0300

    Add SolidRefactored and DependensyResolver projects which consists of some refactoring of Service class.

---
+++ NET.1.SOLID/SolidWorkShop_Refactored/Interfaces/IDataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorkShop_Refactored.Interfaces
+{
+    /// <summary>
+    /// Represents unit of work with a database
+    /// </summary>
+    public interface IDataContext
+    {
+        /// <summary>
+        /// Save data to the database
+        /// </summary>
+        void Save();
+    }
+}

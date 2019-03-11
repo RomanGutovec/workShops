590b039fd4777558671d23a01adb1108457220c0
Roman Gutovec <roman.gutovec@gmail.com>
2019-03-11 19:13:50 +0300

    Remove unnecessary code from console app.

--- NET.11.AsyncTask/LikeWGET/Program.cs
+++ NET.11.AsyncTask/LikeWGET/Program.cs
@@ -1,10 +1,6 @@
 ﻿using LikeWGETLib;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace LikeWGET
 {
@@ -17,18 +13,10 @@ namespace LikeWGET
                 Domain = "by",
                 Extensions = new List<string>()
             };
-
-            LocalCloner cloner = new LocalCloner(conditions, new DownloadService());
-            //Console.WriteLine(cloner.DownLoadByUri("https://stackoverflow.com/questions/599275/how-can-i-download-html-source-in-c-sharp"));
-            //cloner.Clone("https://bipbap.ru/wp-content/uploads/2017/10/0_8eb56_842bba74_XL-640x400.jpg", @"D:\WORKSHOP_HW\Async_await\Net.10.AsyncTask\Downloads");
-            //cloner.Clone("https://pixabay.com/ru/", @"D:\WORKSHOP_HW\Async_await\Net.10.AsyncTask\Downloads2");
-            //Console.WriteLine(  Path.GetExtension("https://pixabay.png") );
-            //cloner.UseExtension("https://pixabay.com/ru/");
+            LocalCloner cloner = new LocalCloner(conditions, new DownloadService());            
 
             var task = cloner.Clone("http://komvak.by/", @"D:\WORKSHOP_HW\Async_await\Net.10.AsyncTask\", 0);
             Console.ReadLine();
         }
-
-
     }
 }
